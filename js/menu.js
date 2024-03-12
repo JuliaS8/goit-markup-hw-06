49db9afce5c65ab564da86debb0a7843327d103a
yuliia_sharapova <juliaa.sharapovaa@gmail.com>
2024-03-12 13:57:57 +0200

    solve problems after checking

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    // Додати атрибут data-menu-open на кнопку відкриття
+    // Додати атрибут data-modal-open на кнопку відкриття
     openMenuBtn: document.querySelector("[data-menu-open]"),
-    // Додати атрибут data-menu-close на кнопку закриття
+    // Додати атрибут data-modal-close на кнопку закриття
     closeMenuBtn: document.querySelector("[data-menu-close]"),
-    // Додати атрибут data-menu на бекдроп модалки
+    // Додати атрибут data-modal на бекдроп модалки
     menu: document.querySelector("[data-menu]"),
   };
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenulBtn.addEventListener("click", toggleMenu);
   function toggleMenu() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.menu.classList.toggle("is-open");
